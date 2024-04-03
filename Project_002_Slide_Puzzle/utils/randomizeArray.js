eee2a69899dbcef8603685865d6267ff5512b4e7
NewGuy8140 <112295678+NewGuy8140@users.noreply.github.com>
2024-04-02 23:04:49 -0400

    did some of what we talked about

--- Project_002_Slide_Puzzle/utils/randomizeArray.js
+++ Project_002_Slide_Puzzle/utils/randomizeArray.js
@@ -1,5 +1,10 @@
 const randomizeArray = function (array) {
-    let newArray = [];
+    for (let i =array.lenght -1; i > 0; i--) {
+        const x =Math.floor(Math.random() * (i + 1));
+        const temp = array[i];
+        array[i] = array[x];
+        array[x] = temp;
+    }
 
     // TO DO: randomize the array and assign to newArray
     return newArray;
