a418f761756ca271102237d50d295bfa540663e2
milo <mroberts18@bristolcc.edu>
2024-04-08 12:55:15 -0400

    Set up handlers for click events on each cell, made alterations to h1 shadow, removed CSS variables used only once and not declared in :root

--- Project_002_Slide_Puzzle/logic.js
+++ Project_002_Slide_Puzzle/logic.js
@@ -9,3 +9,12 @@ const cells = [...document.querySelectorAll('.cell')];
 imagePositions = randomizeArray(imagePositions);
 
 setInitialImages(cells, imagePositions);
+
+// add event listeners to each cell
+cells.map((cell) => {
+    cell.addEventListener('click', () => {
+        // TO DO: Check that a neighboring cell is empty and, if it is, swap both src values
+        // NOTE: This might be easier with things set up as a multidimensional array, but we'd have to go back and redo some things
+        console.log(cell.firstChild);
+    });
+});
