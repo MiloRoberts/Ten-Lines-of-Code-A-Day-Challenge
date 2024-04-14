fe9eb19a285c878fce49b5ba5cc1e44600ee8386
milo <mroberts18@bristolcc.edu>
2024-04-14 08:32:36 -0400

    Added functionality to swap cell images on click

--- Project_002_Slide_Puzzle/logic.js
+++ Project_002_Slide_Puzzle/logic.js
@@ -17,11 +17,31 @@ for (let x = 0; x < 3; x++) {
     }
 }
 
-// add event listeners to each cell
+function swapImageSource(firstImage, secondImage) {
+    secondImage.src = firstImage.src;
+    firstImage.src = "";
+}
+
 cells.map((cell) => {
     cell.addEventListener('click', () => {
-        // TO DO: Check that a neighboring cell is empty and, if it is, swap both src values
-        // NOTE: This might be easier with things set up as a multidimensional array, but we'd have to go back and redo some things
-        console.log(cell.firstChild);
+        let x = parseInt(cell.id.substring(cell.id.length - 1, cell.id.length));
+        let y = parseInt(cell.id.substring(cell.id.length - 2, cell.id.length - 1));
+        // Check that a neighboring cell is empty and, if it is, swap both src values
+        console.log(cellsAsGrid[0][1].firstChild.src);
+        if (x > 0 && cellsAsGrid[x - 1][y].firstChild.getAttribute("src") === "") {
+            swapImageSource(cell.firstChild, cellsAsGrid[x - 1][y].firstChild);
+            // check if won
+        } else if (y > 0 && cellsAsGrid[x][y - 1].firstChild.getAttribute("src") === "") {
+            swapImageSource(cell.firstChild, cellsAsGrid[x][y - 1].firstChild);
+            // check if won
+        }
+        else if (x < cellsAsGrid.length - 1 && cellsAsGrid[x + 1][y].firstChild.getAttribute("src") === "") {
+            swapImageSource(cell.firstChild, cellsAsGrid[x + 1][y].firstChild);
+            // check if won
+        }
+        else if (y < cellsAsGrid.length - 1 && cellsAsGrid[x][y + 1].firstChild.getAttribute("src") === "") {
+            swapImageSource(cell.firstChild, cellsAsGrid[x][y + 1].firstChild);
+            // check if won
+        }
     });
 });
